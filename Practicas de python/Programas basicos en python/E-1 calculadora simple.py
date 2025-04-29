a91b6cdaf946406c8d7e4d2e0d86f15fce85c999
Kleber <kleberdelora1010@gmail.com>
2025-04-29 17:02:21 -0500

    practiras E-1

---
+++ Practicas de python/Programas basicos en python/E-1 calculadora simple.py
@@ -0,0 +1,13 @@
+a = int(input("Ingrese el primer número"))
+
+b = int(input("Ingrese el segundo número"))
+
+# Muestra la suma, la diferencia, el producto y el cociente de los números ingresados.
+
+print("Suma:", a + b)
+
+print("Diferencia:", a - b)
+
+print("Producto", a * b)
+
+print("Cociente:", a / b) # Observacion: Esto puede devolver un valor decimal incluso si los números ingresados son esteros
