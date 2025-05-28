087cc3e06256ee6b1abc3bdd1ff65ed2a0e3cb1e
kleber <kleberdelora1010@gmail.com>
2025-05-28 14:12:14 -0500

    E-3

---
+++ Practicas de python/Programas basicos en python/E-3 Comprobar si un numero es impar o impar.py
@@ -0,0 +1,14 @@
+# Solicita al usuario que ingrese un número.
+
+num = int(input("Ingrese un número: "))
+
+# Comprueba si el número es divisible por 2 para determinar si es par o impar.
+
+if num % 2 == 0:
+ 
+ print("Par")
+
+else:
+ 
+ print("Impar")
+
