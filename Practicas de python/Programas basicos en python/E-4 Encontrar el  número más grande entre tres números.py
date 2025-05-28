9d4fd056e10653504cdfcf07e7a96a30a11ebf36
kleber <kleberdelora1010@gmail.com>
2025-05-28 15:00:00 -0500

    E-4

---
+++ Practicas de python/Programas basicos en python/E-4 Encontrar el  número más grande entre tres números.py
@@ -0,0 +1,10 @@
+a = float(input("Ingrese el primer número: "))
+
+b = float(input("Ingrese el segundo número: "))
+
+c = float(input("Ingrese el tercer número: "))
+
+max_num = max(a, b, c)
+
+print("Número más grande:", max_num)
+30
